71912d1bc121adff23129347039d2a4fa5026ddb
harrisongers <hjgerstenlauer@gmail.com>
2022-03-09 11:31:46 -0500

    import routing and set route to home page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
 
 ReactDOM.render(
   <Router>
   <React.StrictMode>
-    <App />
+    <Routes>
+      <Route path="/" element={<Home/>}>
+        
+      </Route>
+    </Routes>
   </React.StrictMode>
   </Router>,
   document.getElementById('root')
