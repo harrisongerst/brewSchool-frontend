da933868fff56d4bb43f591f89d52d0762298e73
harrisongers <hjgerstenlauer@gmail.com>
2022-03-09 11:41:57 -0500

    add basic nav with ph styling, add Header to Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
-export default function Header() {
+export default function Header(props) {
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    border: "3px solid black",
+    padding: "8px",
+    width: "90%",
+    margin: "auto",
+  };
   return (
-    <div>Header</div>
+    <header>
+      <h1>Brew School</h1>
+      <nav style={navStyle}>
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+      </nav>
+    </header>
   )
 }
