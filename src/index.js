b7a6a192b2c4003dfb89188fa79d6872cfa1684d
harrisongers <hjgerstenlauer@gmail.com>
2022-03-11 11:36:56 -0500

    clean up index, make create route

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router} from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import Show from './pages/Show'
+import Home from './pages/Home';
+import Show from './pages/Show';
+import Create from './pages/Create';
 
 const URL = "https://brew-school.herokuapp.com/"
 
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Route path="/" element={<Home URL={URL} />}>
       </Route>
       <Route path="/recipe/:ID" element={<Show URL={URL}/>} />
+      <Route path="create" element={<Create URL={URL}/>} />
     </Routes>
   </React.StrictMode>
   </Router>,
