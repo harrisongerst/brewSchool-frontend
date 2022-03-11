655d794fe13529db772f3b3d91ca44c7cee178f2
harrisongers <hjgerstenlauer@gmail.com>
2022-03-11 16:13:22 -0500

    refactor nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 
@@ -8,19 +8,12 @@ export default function Header(props) {
     <header>
       <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand as={NavLink} to="/" >Brew School</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+        <Nav.Link as={NavLink} to="/create">New Brew</Nav.Link>
+        <Nav.Link as={NavLink} to="/signup">Signup</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
