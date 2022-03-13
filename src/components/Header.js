1c131b91df96ad296ec323f9c875bdaa02b58a70
harrisongers <hjgerstenlauer@gmail.com>
2022-03-13 18:32:26 -0400

    implement logout button and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 
 
 export default function Header(props) {
+
+  const navigate = useNavigate();
+
+  function logout(){
+    localStorage.removeItem("token")
+    navigate("/", {replace: true})
+  }
+
   return (
     <header>
       <Navbar bg="light" expand="lg">
@@ -14,6 +23,7 @@ export default function Header(props) {
       <Nav className="me-auto">
         <Nav.Link as={NavLink} to="/create">New Brew</Nav.Link>
         <Nav.Link as={NavLink} to="/signup">Signup</Nav.Link>
+        <Button variant="danger" onClick={logout}>Logout</Button>
       </Nav>
     </Navbar.Collapse>
   </Container>
