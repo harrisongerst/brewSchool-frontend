b2cc1804771edd9d47d6c1c84147b283e14c9a06
harrisongers <hjgerstenlauer@gmail.com>
2022-03-13 22:05:20 -0400

    remove unnecessary import

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
