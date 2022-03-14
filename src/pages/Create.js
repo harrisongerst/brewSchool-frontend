7bfd805c61b4184f7e1a2935f4701aa3d36488ce
harrisongers <hjgerstenlauer@gmail.com>
2022-03-13 21:09:06 -0400

    add partial form refactor, add necessary states for dynamic form inputs

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,6 +6,9 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container"
 
 export default function Create(props) {
+
+  const [equipmentInputs, setEquipmentInputs] = useState([1]);
+  const [stepInputs, setStepInputs] = useState([1,2,3]);
   const [newForm, setNewForm] = useState({
     username: "",
     likes: "",
@@ -19,6 +22,12 @@ export default function Create(props) {
     requiredEquipment: [],
     instructions: [],
   });
+
+  function addEquipmentInput(e) {
+    e.preventDefault();
+
+  }
+
   return (
     <div>
       <Header />
@@ -35,7 +44,32 @@ export default function Create(props) {
           <Form.Control type="textarea" as="textarea" rows={2} placeholder="Enter a short description of your brew technique" />
         </Form.Group>
         
-        
+        <Form.Group className="mb-3">
+          <Form.Label>Brew Type</Form.Label>
+          <Form.Check type="radio" label="Pourover" />
+          <Form.Check type="radio" label="Aeropress" />
+          <Form.Check type="radio" label="Other" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Coffee Amount (in grams)</Form.Label>
+          <Form.Control type="number" placeholder="Enter an amount of grams" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Is this for iced coffee?</Form.Label>
+          <Form.Check type="checkbox" label="" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Overall time to brew</Form.Label>
+          <Form.Control type="number" placeholder="Enter the amount of time in seconds" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Title" />
+        </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
