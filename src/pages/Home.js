afcb0e804211311cfc05194b4eccf897586cd07c
harrisongers <hjgerstenlauer@gmail.com>
2022-03-16 00:28:18 -0400

    remove unnecessary import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import BrewCard from "../components/BrewCard";
 import Sidebar from "../components/Sidebar";
 import Container from "react-bootstrap/Container";
